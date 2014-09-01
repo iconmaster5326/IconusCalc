930f8daa71e2b9a1e94a9dd832ddc56715a8b790
Iconmaster <crazybonesj@yahoo.com>
2014-09-01 15:36:38 -0400

    FOR REAL THIS TIME

--- src/com/iconmaster/iconuscalc/gui/KeyInput.java
+++ src/com/iconmaster/iconuscalc/gui/KeyInput.java
@@ -10,10 +10,10 @@ import java.awt.event.KeyEvent;
 public class KeyInput {
     public static final char BACK_SPACE = KeyEvent.VK_BACK_SPACE;
     public static final char ENTER = KeyEvent.VK_ENTER;
-    public static final char LEFT = KeyEvent.VK_LEFT;
-    public static final char RIGHT = KeyEvent.VK_RIGHT;
-    public static final char UP = KeyEvent.VK_UP;
-    public static final char DOWN = KeyEvent.VK_DOWN;
+    public static final char LEFT = (char) -2;
+    public static final char RIGHT = (char) -3;
+    public static final char UP = (char) -4;
+    public static final char DOWN = (char) -5;
     public static final char ESCAPE = KeyEvent.VK_ESCAPE;
     public static final char UNDEFINED = KeyEvent.CHAR_UNDEFINED;
     public static final char DELETE = KeyEvent.VK_DELETE;
